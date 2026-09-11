18cdbebfbebe2d596a0f3c455428e13540834112
briannedalton <bdalton1@wvup.edu>
2026-09-11 16:58:04 -0400

    Updates to the previous errors that were occuring

--- src/UnitConverter/Pages/Conversions.cshtml.cs
+++ src/UnitConverter/Pages/Conversions.cshtml.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Transactions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -15,10 +16,10 @@ public class ConversionsModel : PageModel
 
 
 
-    public void OnGet(string conversionType, string input)
+    public void OnGet()
     {
-        Input = input;
-        ViewData["ConversionType"] = conversionType;
+        ViewData["ConversionType"] = string.Concat(ConversionType.Select(x => Char.IsUpper(x) ? " " + x
+            : x.ToString())).TrimStart(' ');;
         ViewData["Title"] = "Conversions";
         double doubleInput;
 
@@ -31,62 +32,53 @@ public class ConversionsModel : PageModel
             ViewData["ErrorMessage"] = "Input must be a valid number.";
             Console.WriteLine(e.Message);
             return;
-            //throw;
         }
 
 
-        switch (conversionType.ToLower())
+        switch (ConversionType.ToLower())
         {
             case "milestokilometers":
                 UnitOf.Length unit1 = new UnitOf.Length().FromMiles(doubleInput);
                 double x1 = unit1.ToKilometers();
-                string result1 = x1.ToString();
-                Output = result1;
+                Output = x1.ToString();
                 break;
             case "kilometerstomiles":
                 UnitOf.Length unit2 = new UnitOf.Length().FromKilometers(doubleInput);
                 double x2 = unit2.ToMiles();
-                string result2 = x2.ToString();
-                Output = result2;
+                Output = x2.ToString();
                 break;
             case "fahrenheittocelsius":
                 UnitOf.Temperature unit3 = new UnitOf.Temperature().FromFahrenheit(doubleInput);
                 double x3 = unit3.ToCelsius();
-                string result3 = x3.ToString();
-                Output = result3;
+                Output = x3.ToString();
                 break;
             case "celsiustofahrenheit":
                 UnitOf.Temperature unit4 = new UnitOf.Temperature().FromCelsius(doubleInput);
                 double x4 = unit4.ToFahrenheit();
-                string result4 = x4.ToString();
-                Output = result4;
+                Output = x4.ToString();
                 break;
             case "poundstokilograms":
                 UnitOf.Mass unit5 = new UnitOf.Mass().FromPounds(doubleInput);
                 double x5 = unit5.ToKilograms();
-                string result5 = x5.ToString();
-                Output = result5;
+                Output = x5.ToString();
                 break;
             case "kilogramstopounds":
                 UnitOf.Mass unit6 = new UnitOf.Mass().FromKilograms(doubleInput);
                 double x6 = unit6.ToPounds();
-                string result6 = x6.ToString();
-                Output = result6;
+                Output =  x6.ToString();
                 break;
             case "ouncestopounds":
                 UnitOf.Mass unit7 = new UnitOf.Mass().FromOuncesUS(doubleInput);
                 double x7 = unit7.ToPounds();
-                string result7 = x7.ToString();
-                Output = result7;
+                Output = x7.ToString();
                 break;
             case "poundstoounces":
                 UnitOf.Mass unit8 = new UnitOf.Mass().FromPounds(doubleInput);
                 double x8 = unit8.ToOuncesUS();
-                string result8 = x8.ToString();
-                Output = result8;
+                Output = x8.ToString();
                 break;
             default:
-                ViewData["ErroMessage"] = "Unknown conversion type.";
+                ViewData["ErrorMessage"] = "Unknown conversion type.";
                 break;
         }
     }
